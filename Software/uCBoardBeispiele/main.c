8a7cfe0c7e300a5c6994e00f24a9b9055c235eb8
Dario D <dario.duendar@gmail.com>
2020-10-26 08:58:30 +0100

    Neuer Treiber und Beispiele

---
+++ Software/uCBoardBeispiele/main.c
@@ -0,0 +1,108 @@
+/*
+* uCBoardBeispiele.c
+*
+* Created: 26.10.2020 08:43:59
+* Author : Dario Dündar
+*/
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    int16_t x_pos;
+    uint16_t poti1, poti2;
+    uint16_t licht, magnet1, magnet2, temperatur;
+    uint16_t joyStickX, JoyStickY;
+    uint16_t tasten;
+    uint8_t schalter;
+    uint16_t  counter = 0;
+    uint8_t btnJoystick;
+    
+    //Board initialisieren
+    initBoard();
+    //Matrix starten und startanimation zeigen
+    startMatrix();
+    
+    fillMatrix(0);   // Matrix löschen
+    writeZeichenMatrix( 0, 63);
+    writeZeichenMatrix(50, 30);
+    writeTextMatrix(20,"MSW",1);
+    wait5msTick(500);
+    fillMatrix(0);   // Matrix löschen
+    
+    //Dauertext auf LCD anzeigen
+    writeText(0,0,"uC-Board   S1-7:    ");
+    writeText(1,0,"P1/2:         L:    ");
+    writeText(2,0,"M1/2:         C:    ");
+    writeText(3,0,"Jx/y:         T:    ");
+    //LCD-Beleuchtung einschalten
+    lcdLight(10);
+    
+    
+    
+    while(1)
+    {
+        //Eingabe-------------------------------------------------------------------------
+        btnJoystick = (PINE&0b00000100);
+
+        poti1     = readAdc(ADC_08_POTI_1);									// Potentiometer 1
+        poti2     = readAdc(ADC_09_POTI_2);									// Potentiometer 2
+        licht      = readAdc(ADC_12_LICHT);									// Lichtsensor
+        magnet1   = readAdc(ADC_14_MAGNET_1);									// Magnet 1
+        magnet2   = readAdc(ADC_15_MAGNET_2);									// Magnet 2
+        temperatur       = readAdc(ADC_13_TEMPERATUR);									// Temperatur
+        joyStickX	   = readAdc(ADC_10_JOYSTICK_X);									// Joystick x-Achse
+        JoyStickY	   = readAdc(ADC_11_JOYSTICK_Y);									// Joystick y-Achse
+        tasten     = (PINL&0b11000011);			// Tasten 1-4
+        schalter = PINC;
+        
+        //Verarbeitung--------------------------------------------------------------------
+        
+        counter=counter<<1;
+        if(!counter) counter=1;
+        
+        
+        //Ausgabe------------------------------------------------------------------
+        writeZahl(1, 9,poti1,4,0,0);								// Potentiometer 1
+        writeZahl(1, 5,poti2,4,0,0);								// Potentiometer 2
+        writeZahl(1,16,licht,4,0,0);								// Lichtsensor
+        
+        writeZahl(2, 5,magnet1,4,0,0);							// Magnet 1
+        writeZahl(2, 9,magnet2,4,0,0);							// Magnet 2
+        writeZahl(2,16,temperatur,4,0,0);								// Temperatur
+        
+        writeZahl(3, 5,joyStickX,4,0,0);								// Joystick x-Achse
+        writeZahl(3, 9,JoyStickY,4,0,0);								// Joystick y-Achse
+        
+        writeZahl(3,16,tasten,4,0,0);								// Tasten 1-4 und Joystick
+        writeZahl(0,17,schalter,3,0,0);							// DIP_Switch 1-8
+        
+        pwmRgb(joyStickX/2,poti2/2,JoyStickY/2);							// RGB-LED: rot = JS_x, grün = Poti_2, blau = JS_y
+        
+        PORTA = ~counter;
+        PORTB = ~counter>>8;
+        
+        //Matrixanimation bei Tastendruck auf joystick
+        if(btnJoystick)
+        {
+            //Displayausgabe stoppen
+            writeText(0,0,"Matrix Animation....");
+            writeText(1,0,"                    ");
+            writeText(2,0,"                    ");
+            writeText(3,0,"                    ");
+            for(x_pos=-18; x_pos<Anzahl_Spalten; x_pos++)     // 3x6 = 36 entspricht der Länge des Textes in Pixel.
+            {	writeTextMatrix(18,"HELLO",1);
+                writeTextMatrix(x_pos,"_-->",1);    // Schrift schreiben
+                wait5msTick(8);
+                writeTextMatrix(x_pos,"_-->",2);    // Schrift löschen
+            }
+            //Dauertext wieder anzeigen
+            writeText(0,0,"uC-Board   S1-7:    ");
+            writeText(1,0,"P1/2:         L:    ");
+            writeText(2,0,"M1/2:         C:    ");
+            writeText(3,0,"Jx/y:         T:    ");
+        }
+    }
+}
+
---
+++ Software/uCBoardBeispiele/ucBoardDriver.h
@@ -0,0 +1,81 @@
+﻿/*
+ * ucBoardDriver.h
+ *
+ * Created: 26.10.2020 08:44:55
+ *  Author: Dario Dündar
+ */ 
+
+
+#ifndef UCBOARDDRIVER_H_
+#define UCBOARDDRIVER_H_
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#define F_CPU 16000000UL
+#include <util/delay.h>
+
+
+#define ADC_00_X4_PORTF_BIT0    0
+#define ADC_01_X4_PORTF_BIT1    1
+#define ADC_02_X4_PORTF_BIT2    2
+#define ADC_03_X4_PORTF_BIT3    3
+#define ADC_04_NOT_USED         4
+#define ADC_05_NOT_USED         5
+#define ADC_06_NOT_USED         6
+#define ADC_07_NOT_USED         7
+
+#define ADC_08_POTI_1           8
+#define ADC_09_POTI_2           9
+#define ADC_10_JOYSTICK_X       10
+#define ADC_11_JOYSTICK_Y       11
+#define ADC_12_LICHT            12
+#define ADC_13_TEMPERATUR       13
+#define ADC_14_MAGNET_1         14
+#define ADC_15_MAGNET_2         15
+
+
+
+// Vom Benutzer vorgegebene Parameter, die sich je nach Matrix-Grösse unterscheiden
+#define Anzahl_Spalten     56             // Wieviele Spalten hat die Matrix?
+#define Zeichenabstand      1             // Zeichenabstand zwischen zwei Zeichen bei der Textausgabe
+
+
+void initBoard (void);
+
+void start5msTick(void);
+void wait5msTick(uint16_t faktor);
+uint64_t getSystemTime(void);
+
+
+void initRs232 (uint16_t baudratenregister);
+void rs232SendeZeichen(char zeichen);
+void rs232SendeString(char *Text);
+
+void initUsb(void);
+void usbSendeZeichen(char zeichen);
+void usbSendeString(char *Text);
+
+void initAdc(void);
+uint16_t readAdc(uint8_t kanal);
+
+void initRgb(void);
+void pwmRgb(uint16_t Rot,uint16_t Gruen,uint16_t Blau);
+void rgbRot(uint16_t Rot);
+void rgbGruen(uint16_t Gruen);
+void rgbBlau(uint16_t Blau);
+
+void initLcd(void);
+void lcdLight(uint8_t hellighkeit);
+void writeText(uint8_t y_pos, uint8_t x_pos, char *str_ptr);
+void writeZahl(uint8_t x_pos, uint8_t y_pos, uint16_t zahl_v, uint8_t s_vk, uint8_t s_nk, uint8_t komma);
+void clearLcdF(void);
+
+void startMatrix(void);
+void stopMatrix(void);
+void fillMatrix(uint8_t wert);
+void writeTextMatrix(int16_t Matrix_Spalten_Nr, char *str_ptr, uint16_t logic);
+void writeZeichenMatrix(uint16_t Matrix_Spalten_Nr, uint16_t Zeichen_Nr);
+
+
+
+#endif /* UCBOARDDRIVER_H_ */
